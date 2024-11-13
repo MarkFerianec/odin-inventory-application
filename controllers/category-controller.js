5f7f10731b6a9b3bc51fe557f290eae454f26367
Mark Ferianec <mjf397@gmail.com>
2024-11-12 21:36:50 -0700

    Add validation for creating categories

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -5,16 +5,49 @@ exports.getNewCategory = (req, res) => {
   res.render("new-category", { links: links });
 };
 
-exports.postNewCategory = async (req, res, next) => {
-  const { brand, description } = req.body;
+const { body, validationResult } = require("express-validator");
 
-  try {
-    await db.addCategory(brand, description);
-    res.redirect("/categories");
-  } catch (error) {
-    next(error);
-  }
-};
+const alphaErr = "must only contain letters.";
+const brandLengthErr = "must be between 1 and 15 characters.";
+const descriptionLengthErr = "must be between 1 and 30 characters.";
+
+const validateCategory = [
+  body("brand")
+    .trim()
+    .isAlpha()
+    .withMessage(`Brand ${alphaErr}`)
+    .isLength({ min: 1, max: 15 })
+    .withMessage(`Brand ${brandLengthErr}`),
+  body("description")
+    .trim()
+    .isAlpha()
+    .withMessage(`Description ${alphaErr}`)
+    .isLength({ min: 1, max: 30 })
+    .withMessage(`Description ${descriptionLengthErr}`),
+];
+
+exports.postNewCategory = [
+  validateCategory,
+  async (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (errors.isEmpty()) {
+      const { brand, description } = req.body;
+
+      try {
+        await db.addCategory(brand, description);
+        res.redirect("/categories");
+      } catch (error) {
+        next(error);
+      }
+    }
+
+    res.status(400).render("new-category", {
+      links: links,
+      errors: errors.array(),
+    });
+  },
+];
 
 exports.getCategories = async (req, res) => {
   const categories = await db.getAllCategories();
